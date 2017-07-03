a07bf7244620b9c3234dc07544660effc4635fab
GorickUPM <danilosilvaweb@hotmail.com>
2017-07-03 15:02:12 -0300

    issue of lists populated solved

--- src/components/articles.js
+++ src/components/articles.js
@@ -4,7 +4,6 @@ import Styled from 'styled-components'
 import ArticleStore from '../stores/article-store'
 
 const Article = Styled.article`
-        text-align: left;
         margin-left: 25px;
         margin-right: 25px;
         color: DimGrey;
@@ -17,12 +16,19 @@ const Button = Styled.a`
         text-decoration: none;
 `
 
-const Section = Styled.section`
+const ArticlesList = Styled.li`
     border-bottom: 6px solid #bb0e0c;
     border-width: 1px;
     border-collapse: separate;
     border-spacing: 25px;
-    padding-bottom: 85px;
+    padding-bottom: 65px;
+    list-style-type: none;
+`
+
+const Span = Styled.span`
+    display: -webkit-flex;
+    display: flex;
+    text-align: left;
 `
 
 class Articles extends Component {
@@ -31,38 +37,58 @@ class Articles extends Component {
         this.state = { articles: null }
     }
 
-    componentDidMount() {
-        this.setState = {
-            articles: ArticleStore.getAll(),
-        };
+    loadArticles() {
+        this.setState({
+            articles: ArticleStore.getAll()
+        });
+    }
+
+    loadArticleList(articles) {
+        const articlesContent = articles.map((article) => {
+            return <ArticlesList key={article.id}>
+                <h2>{article.title}</h2>
+                <Span>{article.content}</Span><br />
+                <Button href="">Ler mais</Button>
+            </ArticlesList>;
+        });
+        return articlesContent;
+    }
+
+    componentWillMount() {
+        this.loadArticles();
     }
 
     checkingArticles(articles) {
-        if (articles == null || articles == "" || articles == undefined ){
-             return true } 
+        if (articles === null || articles === "" || articles === undefined) {
+            return true
+        }
         else {
-             return false }
+            return false
+        }
     }
-        render() {
+    render() {
 
-            
-            const { articles } = this.state;
+        const { articles } = this.state;
 
-            const articlesContent = checkingArticles(articles) ? "null" : articles.map((article) => {
-                return <li key={article.id}><h1> {article.title} </h1>
-                    {article.content}<Button href="">Ler mais</Button></li>;
-            })
-
+        if (this.checkingArticles(articles)) {
             return (
-                <Section>
+                <section>
+                    <h1>List is empty</h1>
+                </section>
+            )
+        } else {
+            const articlesContent = this.loadArticleList(articles);
+            return (
+                <section>
                     <ul>
                         <Article>
-                            { articlesContent }
+                            {articlesContent}
                         </Article>
                     </ul>
-                </Section>
+                </section>
             )
         }
     }
+}
 
-    export default Articles
+export default Articles
--- src/stores/article-store.js
+++ src/stores/article-store.js
@@ -12,6 +12,10 @@ class ArticleStore extends EventEmitter {
                         2 Congelar(Mas só superfícies pequenas do tamanho de uma latinha e vc fica sem poder usar por um dia).
                         Pirotecnia(Porém é só umas faíscas).
                         4 Cura(Mas se curar alguém perde o equivalente da própria vida, e n pode curar a si mesmo).
+                        5 Acertar o alvo(Vc acert1 Invisibilidade(mas só pra uma pessoa por vez.)
+                        2 Congelar(Mas só superfícies pequenas do tamanho de uma latinha e vc fica sem poder usar por um dia).
+                        Pirotecnia(Porém é só umas faíscas).
+                        4 Cura(Mas se curar alguém perde o equivalente da própria vida, e n pode curar a si mesmo).
                         5 Acertar o alvo(Vc acert
                         `
             },
@@ -46,6 +50,6 @@ class ArticleStore extends EventEmitter {
 
 }
 
-const articleStore = new ArticleStore;
+const articleStore = new ArticleStore();
 
 export default articleStore
