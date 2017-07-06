f0c09821c50dad492d3a6ea4c1280c1200d884dc
GorickUPM <danilosilvaweb@hotmail.com>
2017-07-06 17:20:21 -0300

    about done

--- src/pages/about-page.js
+++ src/pages/about-page.js
@@ -1,26 +1,102 @@
 import React, { Component } from 'react'
 import Styled from 'styled-components'
 
-import Pic from '../assets/foto_roger.jpg'
+/* import Pic from '../assets/foto_roger.jpg' */
+
+import '../components/responsive.css';
 
 
-const Span = Styled.span`
+const Bio = Styled.div`
         color: DimGrey;
         display: -webkit-flex;
         display: flex;
         text-align: left;
 `
 
+/* const Image = Styled.div`
+    display: -webkit-flex;
+    display: flex;
+    width: 200px;
+    height: 200px;
+    border-radius: 100%;
+    background-image: url(${Pic});
+` */
+
+const Span = Styled.span`
+        display: -webkit-flex;
+        display: flex;
+        margin: 20px;
+`
+
+const H1 = Styled.span`
+        font-size: 80px;
+        display: -webkit-flex;
+        display: flex;
+        font-weight: bold;
+        flex-wrap: wrap;
+        margin-left: 10px;
+        margin-right: 10px;
+
+`
+
+const Header = Styled.div`
+        display: -webkit-flex;
+        display: flex;
+        flex-wrap: nowrap;
+        text-align: left;
+`
+
+/*function upd() {
+    let h = "100%";
+    Image.height(h / 5);
+    Image.width(h / 5);
+}
+upd();
+window.onresize = upd;*/
+
+const Title = Styled.span`
+    font-color: #bb0e0c;
+    font-size: 40px;
+    display: -webkit-flex;
+    display: flex;
+    color: #bb0e0c;
+    font-weight: bold;
+`
+
+const SubTitle = Styled.span`
+    font-color: #bb0e0c;
+    display: -webkit-flex;
+    display: flex;
+    justify-content: center;
+    color: #bb0e0c;
+`
+
 
 class AboutPage extends Component {
+
+    /*
+    onMobileScreen(){
+        if (checkingScreen == true){
+            console.log("deu certo porra!")
+        }
+    } */
+
     render() {
         return (
-            <div><img src={Pic} alt="rogelito" height="342" width="322" />
-                <Span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ante vitae odio facilisis cursus sagittis eu velit. Curabitur pharetra in ipsum ut commodo. Ut lacinia, risus id placerat tincidunt, sem purus auctor nunc, id pellentesque mi felis ac tellus. Curabitur elit diam, ornare non finibus quis, sollicitudin sit amet justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla elementum ipsum dolor. Mauris nec odio bibendum justo tincidunt volutpat. Suspendisse porttitor nunc gravida nisi sodales, commodo fringilla augue ornare.
+            <div>
+                <Header>
+                    <H1>Dr. Rogelito D. da Silva</H1><br />
+                </Header>
+                <SubTitle>Advogado, subtenente, pai e um eterno aprendiz.</SubTitle> <br /><br />
+                <Title>Sobre mim</Title>
+
+                <Bio>
+                    <Span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ante vitae odio facilisis cursus sagittis eu velit. Curabitur pharetra in ipsum ut commodo. Ut lacinia, risus id placerat tincidunt, sem purus auctor nunc, id pellentesque mi felis ac tellus. Curabitur elit diam, ornare non finibus quis, sollicitudin sit amet justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla elementum ipsum dolor. Mauris nec odio bibendum justo tincidunt volutpat. Suspendisse porttitor nunc gravida nisi sodales, commodo fringilla augue ornare.
 
 Ut molestie metus quis leo egestas lacinia. Sed rhoncus nulla sed suscipit porta. Proin non erat hendrerit, bibendum urna gravida, tempus purus. Aenean ornare efficitur urna, sed tincidunt ipsum fermentum sit amet. Nullam sit amet consectetur purus. Mauris eu justo nec odio sodales tempor in a urna. Pellentesque at lectus urna. Sed tristique ipsum at bibendum bibendum. Nam vitae posuere lectus. Fusce vel massa dui. Sed et lacus mollis, porta lorem nec, sollicitudin sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce fermentum non arcu non vulputate. Phasellus convallis pulvinar arcu eu elementum.
 
 Aliquam egestas lacus sit amet turpis vestibulum, vel ornare est ultrices. Duis rutrum egestas maximus. Cras ornare nibh sed urna facilisis, eget scelerisque lacus vestibulum. Sed suscipit mi risus, ac malesuada enim commodo eget. Etiam malesuada purus condimentum venenatis fringilla. Pellentesque vehicula auctor felis a venenatis. Nam ut lorem posuere lectus scelerisque scelerisque quis at nisi. Donec consectetur iaculis aliquet. Aliquam tempus condimentum risus.</Span>
+                </Bio>
             </div>)
     }
 }
