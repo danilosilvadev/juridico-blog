ccf2a3b0706735c4dc25ad31c1869418528fb16c
GorickUPM <danilosilvaweb@hotmail.com>
2017-07-12 17:11:50 -0300

    created issue 6

--- src/stores/article-reducer.js
+++ src/stores/article-reducer.js
@@ -3,25 +3,29 @@ import Action from '../actions/article-actions'
 import Counter from '../utils/counter'
 
 
-const ArticleReducer = (state = { id: "", title: "", content: "" }, action) => {
-    switch (action.type) {
-        case ActionTypes.CREATE_ARTICLE: {
-            state = { ...state, id: Counter.increment, title: action.title, content: action.content }
-            var articles = articles.unshift({ id: Counter.increment, title: action.title, content: action.content, list: [{}] });
+class ArticleReducer {
 
+    reducer = (state = { id: "", title: "", content: "" }, action) => {
+        switch (action.type) {
+            case ActionTypes.CREATE_ARTICLE: {
+                state = { ...state, id: Counter.increment, title: action.title, content: action.content }
+                var articles = articles.unshift({ id: Counter.increment, title: action.title, content: action.content, list: [{}] });
+
+            }
+            case ActionTypes.DELETE_ARTICLE: {
+                state = { ...state, id: "", title: "", content: "" }
+                break;
+            }
+            default:
+                return state;
         }
-        case ActionTypes.DELETE_ARTICLE: {
-            state = { ...state, id: "", title: "", content: "" }
-            break;
-        }
-        default:
-            return state;
+
     }
 
+    getAll() {
+        return ArticleReducer.articles.list;
+    }
 }
 
-function getAll() {
-    return ArticleReducer.articles.list;
-}
 
 export default ArticleReducer
